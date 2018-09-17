9ee61bc826fc160d2f716bcd6b8d7b98f6220615
Bianca Dennis <bianca.dennis@caa.columbia.edu>
2018-09-17 14:22:11 -0400

    cleanup

--- dog-catcher/src/BreedList.js
+++ dog-catcher/src/BreedList.js
@@ -9,7 +9,6 @@ export default class BreedList extends Component {
         <div>{view === 'favorites' ? 'Favorites' : 'Caught Breeds'}</div>
         <div style={{display:'flex', flexDirection:'row', justifyContent:'space-around',flexWrap:'wrap'}}>
           {data.map((breed, i) => {
-            console.log()
             return <BreedCard removeFromData={removeFromData} breed={breed} addToFavorites={addToFavorites} favorites={favorites} key={breed.url} url={breed.url} breedName={breed.breedName} index={i}/>
           })}
         </div>
